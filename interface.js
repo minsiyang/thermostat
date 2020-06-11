bc4e05f80a4387d2666f5bd942b2bf2abc1f336f
minsi yang <yangminsi@gmail.com>
2020-06-11 14:47:26 +0100

    trying to change background color

--- interface.js
+++ interface.js
@@ -18,4 +18,27 @@ $(document).ready(function() {
       thermostat.resetTemperature();
       $('#temperature').text(thermostat.temperature);
     })
+
+    $('#ps_mode_on').on('click', function() {
+      thermostat.switchPowerSavingModeOn();
+      $('#power_saving_mode').text("ON");
+      $('#temperature').text(thermostat.temperature);
+    })
+
+    $('#ps_mode_off').on('click', function() {
+      thermostat.switchPowerSavingModeOff();
+      $('#power_saving_mode').text("OFF");
+      $('#temperature').text(thermostat.temperature);
+    })
+
+    $('#temperature').text(thermostat.temperature); 
+      if (thermostat.energyUsage() === 'low-usage'){
+      $('#tempreture').css( 'color', 'green' )
+    } else if (thermostat.energyUsage() === 'medium-usage'){
+      $('#tempreture').css( 'color', 'black' )
+    } else {
+      $('#tempreture').css( 'color', 'red' )
+    }
+ 
+
 })
